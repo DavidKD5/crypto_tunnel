f567aec1eb68bd65fd4eef309666ba64c2cc137d
DavidKD5 <jdbetancourt6@gmail.com>
2022-07-11 18:11:09 -0600

    added crypto search capability

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,25 +1,19 @@
 var current_date = document.querySelector("#current_date");
 current_date.textContent = moment().format("MMMM Do, YYYY");
-var cryptoInput = $("#cryptoSearch");
+var cryptoInput = $("#cryptoInput");
 var deposit = $(".deposit");
-
-// fetch(
-//   "https://api.coingecko.com/api/v3/simple/price?ids=bitcoin&vs_currencies=usd"
-// )
-//   .then((res) => res.json())
-//   .then((data) => {
-//     console.log(data);
-//   });
+var incorrectCrypto = $(".incorrectTicker");
 
 // Jorge's section start
 var crypto_id_list = [];
 var crypto_ticker_list = [];
-var crypto_dict = {};
-const myMap = new Map();
+var myMap = new Map();
 
-let i = 0;
-while (i < 20) {
-  i++;
+// let i = 0;
+// while (i < 1) {
+// i++;
+
+function all_crypto_data() {
   axios({
     method: "get",
     url: "https://api.coingecko.com/api/v3/coins/markets?",
@@ -27,8 +21,8 @@ while (i < 20) {
       vs_currency: "usd",
       order: "market_cap_desc",
       per_page: "250",
-      page: i,
-      sparkline: "false",
+      // page: i,
+      // sparkline: "false",
     },
   }).then((data) => {
     for (var i = 0; i < data.data.length; i++) {
@@ -36,35 +30,38 @@ while (i < 20) {
       crypto_ticker_list.push(data.data[i].symbol);
       myMap.set(data.data[i].symbol, data.data[i].id);
     }
-    // console.log(myMap.get("eth"));
-    console.log(searchCrypto("ethereum"));
   });
+  // }
 }
+all_crypto_data();
 
 function searchCrypto(crypto) {
-  let i = 0;
-  while (i < 1) {
-    i++;
-    axios({
-      method: "get",
-      url: "https://api.coingecko.com/api/v3/coins/markets?",
-      params: {
-        vs_currency: "usd",
-        order: "market_cap_desc",
-        per_page: "250",
-        page: i,
-        sparkline: "false",
-        ids: crypto,
-      },
-    }).then((data) => {
-      console.log(data);
-    });
-  }
+  axios({
+    method: "get",
+    url: "https://api.coingecko.com/api/v3/coins/markets?",
+    params: {
+      vs_currency: "usd",
+      order: "market_cap_desc",
+      per_page: "250",
+      sparkline: "false",
+      ids: crypto,
+    },
+  }).then((data) => {
+    console.log(data);
+  });
 }
 
 function enterInput(e) {
   if (e.keyCode === 13) {
-    searchCrypto(cryptoInput.val());
+    incorrectCrypto.text("");
+    var crypto_name_input = myMap.get(cryptoInput.val());
+    if (myMap.get(cryptoInput.val())) {
+      searchCrypto(crypto_name_input);
+    } else {
+      incorrectCrypto.text(
+        "No crypto ticker found. Please enter correct ticker information."
+      );
+    }
   }
 }
 
